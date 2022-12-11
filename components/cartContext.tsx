67266d7ac0c2055f20db43e0a2c1dbe691f0014c
Michal Sanger <michal.sanger@gmail.com>
2022-12-11 17:50:54 +0100

    Persisted shopping cart

--- components/cartContext.tsx
+++ components/cartContext.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, ReactNode } from "react";
+import { useState, createContext, ReactNode, useEffect } from "react";
 
 export type Cart = Map<number, number>; // key - item ID, value - quantity
 
@@ -20,7 +20,8 @@ export const CartContext = createContext<CartContextType>({
 });
 
 const CartProvider = ({ children }: CartProviderProps) => {
-  const [cart, setCart] = useState<Cart>(new Map());
+  const initialState = new Map();
+  const [cart, setCart] = useState<Cart>(initialState);
 
   const addItem: CartContextType["addItem"] = (id, quantity) => {
     cart.set(id, quantity ?? 1);
@@ -31,6 +32,21 @@ const CartProvider = ({ children }: CartProviderProps) => {
     cart.delete(id);
     setCart(new Map(cart.entries()));
   };
+
+  useEffect(() => {
+    try {
+      const cart = new Map<number, number>(JSON.parse(localStorage.getItem("cart") ?? "[]"));
+      setCart(cart);
+    } catch (e) {
+      console.log(`Stored cart is corrupted`);
+    }
+  }, []);
+  useEffect(() => {
+    if (cart !== initialState) {
+      localStorage.setItem("cart", JSON.stringify(Array.from(cart.entries())));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cart]);
 
   return (
     <CartContext.Provider value={{ cart, addItem, removeItem }}>{children}</CartContext.Provider>
